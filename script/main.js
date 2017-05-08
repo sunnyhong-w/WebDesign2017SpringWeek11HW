c77eec1af7be5bb90b53e7fe73575309aae6f9ac
Sunny Hong <hongmusic0204@gmail.com>
2017-05-08 15:02:54 +0800

    + hello firebase chat

--- script/main.js
+++ script/main.js
@@ -10,10 +10,35 @@ $(document).ready(()=>{
 	};
 	firebase.initializeApp(config);
 
-	var dbRef = firebase.database().ref().child('object');
+	var dbRef = firebase.database().ref();
 
-	dbRef.on('value', (snap) => {
-		$("#title").html(snap.val());
-		console.log(snap.val());
+	var messageField = $("#messageInput");
+	var nameField = $("#nameInput");
+	var messageList = $("#example-message");
+
+	messageField.keypress((e) => {
+		if(e.keyCode == 13)
+		{
+			var username = nameField.val();
+			var message = messageField.val();
+
+			dbRef.child("chat").push({ name: username, text: message });
+			messageField.val('');
+		}
+	})
+
+	dbRef.child("chat").limitToLast(10).on('child_added', (snapshot) => {
+		var data = snapshot.val();
+		var username = data.name || "anonymous";
+		var message = data.text;
+
+		var messageElement = $("<li>");
+		var nameElement = $("<strong class='example-chat-username'></strong>");
+
+		nameElement.text(username);
+		messageElement.text(message).prepend(nameElement);
+
+		messageList.append(messageElement);
+		messageList[0].scrollTop = messageList[0].scrollHeight;
 	})
 })
